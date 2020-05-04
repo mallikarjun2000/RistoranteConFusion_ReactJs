d43b5fd0b1b07e52c5463c4e1a2e121ab0a66036
mallikarjun2000 <arjunmallik093@gmail.com>
2020-05-04 21:10:55 +0530

    Combining Reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,5 @@
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS }  from '../shared/leaders';
-import  { PROMOTIONS } from '../shared/promotions';
+
+
 
 export const initialState = {
     dishes : DISHES,
